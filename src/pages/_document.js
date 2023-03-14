2c861047dc0a3bed03f40dc78b1dc579da7e0cfd
Suraj <surajgupta.college@gmail.com>
2023-03-14 09:51:01 +0530

    added meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  let description = "Upload any image and generate alt text in seconds.";
+  let ogimage = "https://imagealt.vercel.app/imagealt.png";
+  let sitename = "ImageAlt";
+  let title = "ImageAlt - Image to Text";
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={description} />
+        <meta property="og:site_name" content={sitename} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta property="og:image" content={ogimage} />
+        <meta name="twitter:image" content={ogimage} />
+      </Head>
       <body>
         <Main />
         <NextScript />
