6b642064f46df1b07ee0188ed7bbd18451f23a6e
Suraj <surajgupta.college@gmail.com>
2023-03-22 22:02:24 +0530

    fixed error text

--- src/pages/api/generate.js
+++ src/pages/api/generate.js
@@ -38,5 +38,5 @@ export default async function handler(req, res) {
       await new Promise((resolve) => setTimeout(resolve, 500));
     }
   }
-  res.status(200).json(altText ? altText : "Opps! Failed to generate alt text");
+  res.status(200).json(altText ? altText : "Sorry, we can't generate text from this file or image type. It may not be supported. Please try using other image types like 'jpg' or 'png'.");
 }
