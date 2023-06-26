ac39ab20fc64c5572cc07a2efb433690a3fabf32
Suraj <surajgupta.college@gmail.com>
2023-06-26 18:14:49 +0530

    change in <image> to <img>

--- src/components/Output.js
+++ src/components/Output.js
@@ -9,13 +9,10 @@ const Output = ({ data }) => {
                 <div className="flex sm:space-x-8 sm:flex-row flex-col">
                     <div>
                         <h3 className="mb-1 font-medium text-lg text-center text-white w-full">Uploaded Image</h3>
-                        <Image
-                            alt="Your uploaded image"
+                        <img
                             src={!data.files ? "/example.webp" : data.files}
-                            className="w-full object-cover h-96 rounded-2xl"
-                            width={475}
-                            height={475}
-                        />
+                            alt="Your uploaded image"
+                            className="w-full object-cover h-96 rounded-2xl" />
                     </div>
                     <div className="sm:mt-0 mt-8 w-full md:w-[500px]">
                         <h3 className="mb-1 font-medium text-center text-lg text-white ">Image Alt Text </h3>
