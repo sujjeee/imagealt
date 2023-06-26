d295690c7ccf124a0505e24f5ef5ce959aa1594a
Suraj <surajgupta.college@gmail.com>
2023-06-26 18:23:23 +0530

    fixed bug

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      'upcdn.io'
-    ],
-  }
 }
 
 module.exports = nextConfig
